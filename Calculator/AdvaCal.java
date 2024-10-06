f07accce3c1403385dcacf029faa18f5225dea69
BHYRAVA-SRI-HARSHA <harsha.bs0542@gmail.com>
2024-10-06 21:53:18 +0530

    Add files via upload

---
+++ Calculator/AdvaCal.java
@@ -0,0 +1,11 @@
+package Calculator;
+
+public class AdvaCal extends Calc{
+    public int mul(int n1, int n2){
+        return n1*n2;
+   }
+
+   public double div(int n1, int n2){
+        return n1/n2;
+   }
+}
